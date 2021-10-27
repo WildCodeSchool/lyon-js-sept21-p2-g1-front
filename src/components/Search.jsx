c4a92bdf541585e3fa98fc6ac97e95cc4a8ab100
kalilBelgoumri <kalil.belgoumri@gmail.com>
2021-10-27 20:58:56 +0200

    i have add the search bar

---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Input } from 'semantic-ui-react';
+
+const Search = () => {
+  return (
+    <div>
+      <Input fluid icon="search" placeholder="Search..." />
+    </div>
+  );
+};
+
+export default Search;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Input } from 'semantic-ui-react';
 import React from 'react';
 import img from '../assets/parking.jpg';
 import Orange from '../components/Orange';
@@ -5,11 +6,16 @@ import Footer from '../components/Footer';
 
 function Home() {
   return (
-    <div className="w-full absolute h-screen -z-1 top-0">
-      <img src={img} alt="parking" />
-      <Orange />
-      <Footer />
-    </div>
+    <>
+      <div className="absolute right-10 left-10 top-28">
+        <Input fluid icon="search" placeholder="Search..." />
+      </div>
+      <div className="w-full absolute h-screen -z-1 top-0">
+        <img src={img} alt="parking" />
+        <Orange />
+        <Footer />
+      </div>
+    </>
   );
 }
 
