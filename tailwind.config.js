e263d49105ce1ac1577aace9f9b11249a704cb72
kalilBelgoumri <kalil.belgoumri@gmail.com>
2021-10-27 20:59:51 +0200

    i have remove the component Search

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 /* eslint-disable */
 
 module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            zIndex: {
-                '-1': '-1',
-            },
-            backgroundColor: (theme) => ({
-                ...theme('colors'),
-                primary: '#FCCF31',
-                secondary: '#1D1A1A',
-            }),
-        },
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      zIndex: {
+        '-1': '-1',
+      },
+      backgroundColor: (theme) => ({
+        ...theme('colors'),
+        primary: '#FCCF31',
+        secondary: '#1D1A1A',
+      }),
     },
-    // theme: {
-    //     extend: {
-    //         fontFamily: {
-    //             sans: ['Fira Sans', 'sans-serif'],
-    //         },
-    //     },
-    // },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  },
+  // theme: {
+  //     extend: {
+  //         fontFamily: {
+  //             sans: ['Fira Sans', 'sans-serif'],
+  //         },
+  //     },
+  // },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
