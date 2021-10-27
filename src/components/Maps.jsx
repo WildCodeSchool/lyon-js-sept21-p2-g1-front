6aa0d5bf9fdb1d3534a2ff2550693034cfb22a67
bahstiien <bastien.lecalvez@gmail.com>
2021-10-27 21:35:15 +0200

    maps without geoloc

---
+++ src/components/Maps.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px',
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
+
+function Maps() {
+  return (
+    <LoadScript googleMapsApiKey="AIzaSyAllxS_YzDHgJIGlXJlvYszWVM7OniCyUU">
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+        {/* Child components, such as markers, info windows, etc. */}
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  );
+}
+
+export default React.memo(Maps);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Maps from '../components/Maps';
 
 function Home() {
   return (
     <div className="bg-red-700">
       <h1>Home</h1>
+      <Maps />
     </div>
   );
 }
