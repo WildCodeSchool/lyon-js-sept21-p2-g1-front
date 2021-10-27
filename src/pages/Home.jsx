37062fe324b0a0678a25993a814c59bb35d8d140
bahstiien <bastien.lecalvez@gmail.com>
2021-10-27 12:26:06 +0200

    add the google maps with bug:)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Geolocation from '../components/GeoLocation';
+import LocationSearchModal from '../components/GeoLocation';
 
 function Home() {
   return (
     <div className="bg-red-700">
       <h1>Home</h1>
-      <Geolocation />
+      <LocationSearchModal />
     </div>
   );
 }
