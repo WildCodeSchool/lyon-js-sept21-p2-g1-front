94f65574aa79ef0551ff47d16313ca7d951298dc
kalilBelgoumri <kalil.belgoumri@gmail.com>
2021-10-28 16:03:50 +0200

    I have added props in CardHome

--- src/components/CardHome.jsx
+++ src/components/CardHome.jsx
@@ -1,14 +1,18 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import img from '../assets/parkingS.png';
 
-function CardHome() {
+function CardHome(props) {
   return (
-    <div className="bg-primary rounded-3xl shadow-2xl w-11/12 h-96 flex items-center justify-center flex-col">
-      <h1 className="text-white text-center rounded-t-xl">
-        {' '}
-        Parkings surveillés{' '}
-      </h1>
-      <img className="inset-x-14" src={img} alt="parkingS" />
+    <div className="bg-primary rounded-3xl shadow-2xl w-11/12 h-96">
+      <div className="picture__text  flex items-center justify-center flex-col">
+        <h1 className="text-white text-center rounded-t-xl mb-5 p-5">
+          {props.name}
+        </h1>
+        <img className="" src={img} alt="parkingS" />
+      </div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,19 +5,24 @@ import img from '../assets/parking.jpg';
 import Footer from '../components/Footer';
 import CardHome from '../components/CardHome';
 import SecondaryColor from '../components/BackgroundColors';
+import parking from '../assets/parkingD.png';
 
 function Home() {
   return (
     <>
       <div className="overflow-hidden">
         <div className="visible md:invisible card__Home flex flex-wrap justify-center items-center inset-x-0 top-96 absolute">
-          <CardHome />
+          <CardHome name="Parking surveillé" />
+          <CardHome
+            name="Stationnement partagés"
+            className="mb-10 <img src={Logo}/>"
+          />
         </div>
         <div className="absolute right-10 left-10 top-28">
           <Input fluid icon="search" placeholder="Search..." />
         </div>
         <div className="w-full absolute h-screen -z-1 top-0">
-          <img src={img} alt="parking" />
+          <img src={parking} alt="parking" />
           <SecondaryColor />
           <Footer />
         </div>
