5fb8d269d4c093f11cb0334c4b560ba5386b537d
kalilBelgoumri <kalil.belgoumri@gmail.com>
2021-10-28 15:42:09 +0200

    i have delete the orange file

--- src/components/Orange.jsx
+++
@@ -1,7 +0,0 @@
-const Orange = () => {
-  return (
-    <div className="invisible md:visible bg-primary w-full h-screen"> </div>
-  );
-};
-
-export default Orange;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,32 @@
 /* eslint-disable */
 
 module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            zIndex: {
-                '-1': '-1',
-            },
-            backgroundColor: (theme) => ({
-                ...theme('colors'),
-                primary: '#FF9F29',
-                secondary: '#1D1A1A',
-            }),
-            margin: {
-                '-540': '-540px',
-            },
-        },
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      zIndex: {
+        '-1': '-1',
+      },
+      backgroundColor: (theme) => ({
+        ...theme('colors'),
+        primary: '#FF9F29',
+        secondary: '#1D1A1A',
+      }),
+      margin: {
+        '-540': '-540px',
+      },
     },
-    // theme: {
-    //     extend: {
-    //         fontFamily: {
-    //             sans: ['Fira Sans', 'sans-serif'],
-    //         },
-    //     },
-    // },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  },
+  // theme: {
+  //     extend: {
+  //         fontFamily: {
+  //             sans: ['Fira Sans', 'sans-serif'],
+  //         },
+  //     },
+  // },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
