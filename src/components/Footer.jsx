e6749610571da15e8003502323028e732709d02c
kalilBelgoumri <kalil.belgoumri@gmail.com>
2021-10-28 21:26:30 +0200

    i have add Rating in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Rating } from 'semantic-ui-react';
 
 const Footer = () => (
-  <div className="bg-secondary w-full h-72">
-    <p>This is some content in sticky footer</p>
+  <div className="bg-secondary w-full h-72 flex justify-center items-center">
+    <Rating maxRating={5} defaultRating={3} icon="star" size="massive" />
   </div>
 );
 
