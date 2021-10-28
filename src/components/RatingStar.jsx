15c886f9aa751ee0b0582317255ba5c6f6db6196
Florence Martin <florence.martin469@gmail.com>
2021-10-28 16:07:51 +0200

    CSS

--- src/components/RatingStar.jsx
+++ src/components/RatingStar.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 import { Rating } from 'semantic-ui-react';
 
-const RatingStar = () => <Rating icon="star" defaultRating={3} maxRating={4} />;
+const RatingStar = () => (
+  <Rating
+    className="md:p32 space-y-16 mx-16 "
+    icon="star"
+    defaultRating={5}
+    maxRating={5}
+  />
+);
 
 export default RatingStar;
--- src/pages/Rating.jsx
+++ src/pages/Rating.jsx
@@ -1,6 +1,6 @@
 import { Button, Checkbox, Form, Input, TextArea } from 'semantic-ui-react';
 import RatingStar from '../components/RatingStar';
-import img from '../assets/parking-vide.png';
+import img from '../assets/parking.jpg';
 
 const Rating = () => {
   return (
@@ -8,16 +8,22 @@ const Rating = () => {
       <img className="object-center md:object-center" src={img} alt="parking" />
 
       <Form className="md:flex space-y-8 mx-16 justify-center flex-col">
-        <p className="justify-center font-semibold">
+        <p className="text-center justify-center font-semibold">
           “Tailwind CSS is the only framework that I've seen scale on large
           teams. It’s easy to customize, adapts to any design, and the build
           size is tiny.”
         </p>
-        <Form.Field control={Input} label="Name" placeholder="Name" />
+        <Form.Field
+          className="shadow-xl rounded-3xl border-gray-400"
+          control={Input}
+          label="Name"
+          placeholder="Name"
+        />
 
         <RatingStar />
 
         <Form.Field
+          className="shadow-xl rounded-3xl border-gray-400"
           control={TextArea}
           label="Message"
           placeholder="Tell us more about us..."
@@ -26,7 +32,6 @@ const Rating = () => {
           control={Checkbox}
           label="I agree to the Terms and Conditions"
         />
-
         <Form.Field control={Button}>Submit</Form.Field>
       </Form>
     </div>
