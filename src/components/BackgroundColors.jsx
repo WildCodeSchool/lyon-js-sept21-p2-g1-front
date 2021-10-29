ec91ef7d4084c622dc0c07dceeb5b559284f260b
kalilBelgoumri <kalil.belgoumri@gmail.com>
2021-10-29 15:08:02 +0200

    i have modified Home Footer and BackgroundColors

--- src/components/BackgroundColors.jsx
+++ src/components/BackgroundColors.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 function SecondaryColor() {
   return (
     <div className="secondary__clolor visible md:invisible bg-secondary w-full h-screen">
-      <div className="primary__color invisible md:visible bg-primary w-full h-screen">
-        {' '}
-      </div>
+      <div className="primary__color invisible md:visible bg-primary w-full h-screen" />
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable no-unused-vars */
 import { Input } from 'semantic-ui-react';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import img from '../assets/parking.jpg';
 import Footer from '../components/Footer';
 import CardHome from '../components/CardHome';
@@ -10,13 +11,20 @@ import parkingD from '../assets/parkingHomeD.png';
 import parkingS from '../assets/parkingHomeS.png';
 
 function Home() {
+  const [searchInput, setSearchInput] = useState('');
+  const [validateSearch, setValidateSearch] = useState('');
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    return () => {};
+  }, []);
+
   return (
     <>
       <div className="overflow-hidden">
         <div className="visible md:invisible card__Home flex flex-wrap justify-center items-center inset-x-0 top-96 absolute">
           <CardHome name="Parking surveillé" parking={parkingS} />
         </div>
-
         <div className="visible md:invisible card__partage flex justify-center absolute w-full bottom-10">
           <img
             className="-my-6 absolute cursor-pointer"
@@ -31,8 +39,21 @@ function Home() {
 
           <CardHome name="Stationnement partagés" parking={parkingD} />
         </div>
+
         <div className="absolute right-10 left-10 top-28">
-          <Input fluid icon="search" placeholder="Search..." />
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              setValidateSearch(searchInput);
+            }}
+          >
+            <Input
+              fluid
+              icon="search"
+              placeholder="Search..."
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+          </form>
         </div>
         <div className="w-full absolute h-screen -z-1 top-0">
           <img src={img} alt="parking" />
