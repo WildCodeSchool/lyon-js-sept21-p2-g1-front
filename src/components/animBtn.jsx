fce5a4eeea03448a381696399dad22c84dd4ea2d
bahstiien <bastien.lecalvez@gmail.com>
2021-11-03 11:34:46 +0100

    resolve the route on the app.js

--- src/components/animBtn.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Button } from 'semantic-ui-react';
-
-const ButtonExampleLoading = () => <div>A</div>;
-
-export default ButtonExampleLoading;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import ButtonExampleLoading from '../components/animBtn';
 
 function Home() {
   return (
     <div className="bg-red-700">
       <h1>Home</h1>
-      <ButtonExampleLoading />
     </div>
   );
 }
