58dca66f1e182bdda786b8e9783ab2e6bfe7d807
bahstiien <bastien.lecalvez@gmail.com>
2021-11-10 14:46:32 +0100

    add css on the legend box in the maps

--- src/components/LegendMaps.jsx
+++ src/components/LegendMaps.jsx
@@ -2,24 +2,32 @@ import React from 'react';
 
 const LegendMaps = () => {
   return (
-    <div className="flex items-center justify-center">
-      <div className="boxLegend m-4 border-2 rounded-3xl items-center shadow-2xl w-30">
-        <div className="flex items-center m-8 justify-center">
-          <img src="iconHandi.png" alt="pmr" />
-          <p> Stationnement accessible aux personnes à mobilité réduite</p>
+    <div className="boxInfo flex items-center justify-center">
+      <div className="boxLegend m-4 border-2 rounded-3xl  shadow-2xl ">
+        <div className="flex items-center justify-center">
+          <img src="iconHandi.png" alt="pmr" className="w-8 m-4" />
+          <p className="text-xs">
+            Stationnement accessible aux personnes à mobilité réduite
+          </p>
         </div>
 
-        <div className="flex items-center m-8 justify-center">
-          <img src="iconHeight.png" alt="hauteur véhicule" />
-          <p> La hauteur du parking est inférieure à 2m</p>
+        <div className="flex items-center justify-center">
+          <img
+            src="iconHeight.png"
+            alt="hauteur véhicule"
+            className="w-8 m-4"
+          />
+          <p className="text-xs">La hauteur du parking est inférieure à 2m</p>
         </div>
 
-        <div className="flex items-center m-8 justify-center">
-          <img src="iconCarShare.png" alt="autopartage" className="w-12" />
-          <p> Stationnement accessible aux véhicules en autopartage</p>
+        <div className="flex items-center justify-center">
+          <img src="iconCarShare.png" alt="autopartage" className="w-8 m-4" />
+          <p className="text-xs">
+            Stationnement accessible aux véhicules en autopartage
+          </p>
         </div>
       </div>
-      <div className="DataBox flex items-center m-8 justify-center flex-col border-2 rounded-3xl shadow-2xl">
+      <div className="DataBox flex items-center m-8 justify-center flex-col">
         <img src="grandLyon.jpeg" alt="pmr" />
         <p> Data de la métropole de Lyon</p>
       </div>
