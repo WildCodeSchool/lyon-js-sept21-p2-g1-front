7f7248c0b1fa5b93905abf133dc09e4fc5cc4e8b
bahstiien <bastien.lecalvez@gmail.com>
2021-11-10 16:04:43 +0100

    add css on the buttton geoloc

--- src/components/LegendMaps.jsx
+++ src/components/LegendMaps.jsx
@@ -5,13 +5,13 @@ const LegendMaps = () => {
     <div className="boxInfo flex items-center justify-center">
       <div className="boxLegend m-4 border-2 rounded-3xl  shadow-2xl ">
         <div className="flex items-center justify-center">
-          <img src="iconHandi.png" alt="pmr" className="w-8 m-4" />
+          <img src="iconHandi.png" alt="pmr" className="w-1 m-4" />
           <p className="text-xs">
             Stationnement accessible aux personnes à mobilité réduite
           </p>
         </div>
 
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center mt-3 m-4">
           <img
             src="iconHeight.png"
             alt="hauteur véhicule"
@@ -20,8 +20,8 @@ const LegendMaps = () => {
           <p className="text-xs">La hauteur du parking est inférieure à 2m</p>
         </div>
 
-        <div className="flex items-center justify-center">
-          <img src="iconCarShare.png" alt="autopartage" className="w-8 m-4" />
+        <div className="flex items-center justify-center mt-3">
+          <img src="iconCarShare.png" alt="autopartage" className="w-3 m-4" />
           <p className="text-xs">
             Stationnement accessible aux véhicules en autopartage
           </p>
@@ -29,7 +29,9 @@ const LegendMaps = () => {
       </div>
       <div className="DataBox flex items-center m-8 justify-center flex-col">
         <img src="grandLyon.jpeg" alt="pmr" />
-        <p> Data de la métropole de Lyon</p>
+        <p>
+          Les datas sont fournies à titre indicatif par la métropole de Lyon.
+        </p>
       </div>
     </div>
   );
