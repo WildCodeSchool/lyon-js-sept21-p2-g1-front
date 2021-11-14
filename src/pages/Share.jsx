3a9d3b0fac2e49252ba2dd774002d589ded51f3f
bahstiien <bastien.lecalvez@gmail.com>
2021-11-14 21:32:26 +0100

    add css in my form

--- src/pages/Share.jsx
+++ src/pages/Share.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import { Widget } from '@uploadcare/react-widget';
-import { Button, Checkbox, Form } from 'semantic-ui-react';
+import { Button, Checkbox, Form, FormField } from 'semantic-ui-react';
 import icon from '../assets/gps.png';
+import img from '../assets/carParkFull.jpg';
+import '../share.css';
 
+const customBtn = () => ({
+  buttons: {
+    choose: {
+      files: {
+        one: 'upLoadez votre photo',
+      },
+    },
+  },
+});
 function Share() {
   return (
-    <div>
-      <p>
+    <div className="flex flex-col justify-center">
+      <div className="lg:h-40">
+        <img
+          className="object-cover h-48 w-full object-center"
+          src={img}
+          alt="parking"
+        />
+      </div>
+      <p className="p-20 flex justify-center">
         Vous liberez une place de parking dans la rue ? Vous voyez une place
         disponible ? Partagez l'information avec une photo pour la communauté de
         Space-Park
       </p>
 
       <Form>
-        <Form.Field>
+        <Form.Field className="flex justify-center w-3/4">
           <label htmlFor="name" className="m-2">
             Votre nom
             <input
@@ -24,6 +42,8 @@ function Share() {
               placeholder="Votre nom"
             />
           </label>
+        </Form.Field>
+        <FormField className="flex justify-center w-3/4">
           <div>
             <p> Géolocalisez moi : </p>
             <img
@@ -51,17 +71,27 @@ function Share() {
               }}
             />
           </div>
-        </Form.Field>
-        <label htmlFor="file" className="m-2">
-          Votre photo :{/* <input id="file" name="file" /> */}
-          <Widget publicKey="Yc80aef425dbde274da96" id="file" name="file" />
-        </label>
-        <Form.Field>
+        </FormField>
+
+        <FormField className="flex justify-center w-3/4">
+          {/* <label htmlFor="file" className="m-2">
+            Votre photo : <input id="file" name="file" /> */}
+          <Widget
+            publicKey="bf85fdb4cd586983b065"
+            id="file"
+            name="file"
+            localeTranslations={customBtn()}
+          />
+        </FormField>
+        <Form.Field className="flex justify-center w-3/4">
           <Checkbox label=" J'ai lu et j'accepte les Conditions Générales d'Utilisation de Space Park" />
         </Form.Field>
-        <Button type="submit">Partagez votre Place</Button>
+        <Button className="submitBtn" type="submit">
+          Partagez votre Place
+        </Button>
       </Form>
     </div>
   );
 }
+
 export default Share;
