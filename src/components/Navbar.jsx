83754dc0411a69855d0db49e8fa2aa0bd8f56fc9
bahstiien <bastien.lecalvez@gmail.com>
2021-11-19 10:38:34 +0100

    update burger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,21 +9,21 @@ const Navbar = () => {
         <div className="hidden md:flex">
           <img src={logo} alt="Logo" />
         </div>
-        <div className="flex justify-center ">
+        <div>
           <ul className="w-full m-966 ml-0 justify-self-start flex mr-12 m md:flex-row md:m-auto md:pl-0">
             <li className="text-xl md:text-2xl">
               <Link to="/">Accueil</Link>
             </li>
-            <li className="text-xl font-bold md:text-2xl">
+            <li className="text-xl font-bold md:text-lg">
               <Link to="/share">Partage</Link>
             </li>
-            <li className="text-xl font-bold md:text-2xl">
+            <li className="text-xl font-bold md:text-lg">
               <Link to="/rating">Avis</Link>
             </li>
-            <li className="text-xl font-bold md:text-2xl">
+            <li className="text-xl font-bold md:text-lg">
               <Link to="/map">Carte</Link>
             </li>
-            <li className="text-xl font-bold md:text-2xl">
+            <li className="text-xl font-bold md:text-lg">
               <Link to="/aboutUs">A propos</Link>
             </li>
           </ul>
